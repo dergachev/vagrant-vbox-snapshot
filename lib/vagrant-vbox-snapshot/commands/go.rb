2735fda5cbb0fb412c3a55132a029877435d112d
Alex Dergachev <alex@evolvingweb.ca>
2013-06-13 15:56:44 -0400

    'snapshot go' now warns if shared folders changed after restore; supports --reload option

--- lib/vagrant-vbox-snapshot/commands/go.rb
+++ lib/vagrant-vbox-snapshot/commands/go.rb
@@ -1,14 +1,49 @@
+require 'pp'
+
 module VagrantPlugins
   module VBoxSnapshot
     module Command
       class Go < Vagrant.plugin(2, :command)
+        def get_shared_folders(vm_id)
+          shared_folders = []
+          info = `VBoxManage showvminfo #{vm_id} --machinereadable`
+          info.split("\n").each do |line|
+            if line =~ /^SharedFolderNameMachineMapping\d+="(.+?)"$/
+              shared_folders << $1.to_s
+            end
+          end
+          return shared_folders
+        end
+
+        def before_restore(vm_id)
+          @shared_folders_before = get_shared_folders(vm_id)
+        end
+
+        def after_restore(vm_id)
+          @shared_folders_after = get_shared_folders(vm_id)
+          if @shared_folders_before != @shared_folders_after
+            missing_folders = @shared_folders_before - @shared_folders_after
+            @env.ui.warn("Synced folders have changed after restoring snapshot. Consider running 'vagrant reload'.")
+            @env.ui.warn("   Before restore: #{@shared_folders_before}")
+            @env.ui.warn("    After restore: #{@shared_folders_after}")
+          end
+        end
+
         def execute
           options = {}
+          options[:reload] = false
 
           opts = OptionParser.new do |opts|
             opts.banner = "Go to specified snapshot"
             opts.separator ""
             opts.separator "Usage: vagrant snapshot go <SNAPSHOT_NAME>"
+
+            # build_snapshot_options(opts, options)
+
+            opts.on("-r", "--reload", "reconfigure restored machine config (eg. shared folders) is consistent with Vagrantfile. (slower)") do |reload|
+              options[:reload] = reload
+            end
+
           end
           # Parse the options
           argv = parse_options(opts)
@@ -22,9 +57,22 @@ module VagrantPlugins
           with_target_vms do |machine|
             vm_id = machine.id
             snapshot_name = argv[0]
+
+            before_restore(vm_id)
+            
             system "VBoxManage controlvm #{vm_id} poweroff"
-            system "VBoxManage snapshot  #{vm_id} restore #{snapshot_name}"
-            system "VBoxManage startvm   #{vm_id} --type headless"
+            system "VBoxManage snapshot #{vm_id} restore #{snapshot_name}"
+
+            if options[:reload]
+              @env.ui.info("Reloading VM")
+              machine.action(:reload, :provision_enabled => false)
+            else
+              @env.ui.info("Starting VM from restored snapshot (#{snapshot_name})")
+              @logger.info("starting VM")
+              system "VBoxManage startvm #{vm_id} --type headless"
+            end
+
+            after_restore(vm_id)
           end
         end
       end

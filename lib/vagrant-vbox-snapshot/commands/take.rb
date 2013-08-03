3b8f6c998025ff0203c752c25ff08a9ac4b46d23
Fabio Rehm <fgrehm@gmail.com>
2013-08-03 12:27:21 -0300

    Add multi-vm support for `take` command

--- lib/vagrant-vbox-snapshot/commands/take.rb
+++ lib/vagrant-vbox-snapshot/commands/take.rb
@@ -8,20 +8,21 @@ module VagrantPlugins
           opts = OptionParser.new do |opts|
             opts.banner = "Take snapshot"
             opts.separator ""
-            opts.separator "Usage: vagrant snapshot take <SNAPSHOT_NAME>"
+            opts.separator "Usage: vagrant snapshot take [vm-name] <SNAPSHOT_NAME>"
           end
           # Parse the options
           argv = parse_options(opts)
           return if !argv
 
-          if !argv[0]
+          unless [1, 2].include?(argv.size)
             @env.ui.info(opts.help, :prefix => false)
             return
           end
 
-          with_target_vms do |machine|
+          vm_name, snapshot_name = (argv.size == 1 ? [nil, argv[1]] : [argv[0], argv[1]])
+
+          with_target_vms(vm_name, single_target: true) do |machine|
             vm_id = machine.id
-            snapshot_name = argv[0]
             system "VBoxManage snapshot #{vm_id} take #{snapshot_name} --pause"
           end
         end

7a011ef94f7c4e662ee538ef243fa255b363dc47
Alex Dergachev <alex@evolvingweb.ca>
2014-10-06 17:36:50 -0400

    'vagrant snapshot list' suppress error if no snapshots; fixes #17

--- lib/vagrant-vbox-snapshot/commands/list.rb
+++ lib/vagrant-vbox-snapshot/commands/list.rb
@@ -17,8 +17,13 @@ module VagrantPlugins
           with_target_vms(argv, single_target: true) do |machine|
             puts "Listing snapshots for '#{machine.name}':"
 
-            res = machine.provider.driver.execute("snapshot", machine.id, "list", "--details") do |type, data|
-              machine.env.ui.info(data, :color => :green) if (type == :stdout)
+            begin
+              res = machine.provider.driver.execute("snapshot", machine.id, "list", "--details") do |type, data|
+                @suppress_error = TRUE if data == "This machine does not have any snapshots\n"
+                machine.env.ui.info(data, :color => :green) if (type == :stdout)
+              end
+            rescue Vagrant::Errors::VBoxManageError => e
+              raise e unless @suppress_error
             end
           end
         end
